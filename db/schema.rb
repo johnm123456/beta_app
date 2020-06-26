cbb15122f4c0cfad60cd5fd4ceb61270c696e040
Jovan Miskovic <iammiskovic@gmail.com>
2020-06-26 11:53:22 +0200

    Add User model and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_203231) do
+ActiveRecord::Schema.define(version: 2020_06_26_094910) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
