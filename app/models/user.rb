eef51a7a68f1fe62a70249d28825c32a7c329f2e
Jovan Miskovic <iammiskovic@gmail.com>
2020-06-26 12:31:20 +0200

    add alidations to user and format the email adress with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
-Class User < ApplicationRecord
-
-end
+ class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 5, maximum: 15 }
+  VALID_SAVE_FORMAT_FOR_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 50 },
+            format: { with: VALID_SAVE_FORMAT_FOR_EMAIL }
+ end
